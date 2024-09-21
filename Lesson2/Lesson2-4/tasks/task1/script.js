1f520f0eed39e57ef5bade03eb79050bc35f10b2
Daniil Jasper <jasper@deeplay.io>
2024-09-21 18:48:40 +0600

    feat: [L-4] homework

---
+++ Lesson2/Lesson2-4/tasks/task1/script.js
@@ -0,0 +1,15 @@
+'use strict'
+// Объявление переменной admin
+let admin;
+
+// Объявление переменной name
+let name;
+
+// Инициализация переменной name
+name = "Джон";
+
+// Инициализация перменной admin как name
+admin = name;
+
+// Вывод переменной admin через функцию alert
+alert(admin);
---
+++ Lesson2/Lesson2-4/tasks/task2/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//  Объявление переменной earth
+let earth = 'Земля';
+
+// Объвление переменной currentUserName
+let currentUserName = 'dany';
+
+// Вывод переменной earth и currentUserName через функцию alert
+alert('Имя: ' +  currentUserName + ' с планеты ' + earth);
