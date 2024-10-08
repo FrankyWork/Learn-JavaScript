ef8279e6bd63ee7c62e81f2c7542677058acd46a
Daniil Jasper <jasper@deeplay.io>
2024-10-08 08:31:17 +0600

    feat: [L-7] Базовые операторы, математика

---
+++ Lesson2/Lesson2-7/scripts/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+ Инкремент/декремент
+*/
+
+let counter = 10;
+alert(`Инициализируется значение ${counter}`)
+
+let inc = counter;
+alert(`Инкруемнет ${++inc}`)
+
+let dec = counter;
+alert(`Декремент ${--dec}`)
+
+
